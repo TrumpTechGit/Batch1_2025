ba969c9f8c26aee9647d5d3861650604c7d908e7
TrumpTechGit <Ramsamsani@Ram.mshome.net>
2025-06-26 08:46:49 +0530

    26/06 functional interfaces and Object class

--- OOPs_Implementaton/src/in/trump/interfaces/Vehicle.java
+++ OOPs_Implementaton/src/in/trump/interfaces/Vehicle.java
@@ -1,11 +1,20 @@
 package in.trump.interfaces;
 
+@FunctionalInterface
 public interface Vehicle {
+	//Invalid '@FunctionalInterface' annotation; Vehicle is not a functional interface
 	
 	void serviceVehicle(int duration);
 	
+	//void playMusic();
+	
 	default void startVehicle() {
 		System.out.println("vehicle started !");
+	}
+	
+	//static methods,variable can be called using class name
+	static void driveVehicle() {
+		System.out.println("Driver/Owner is driving");
 	}
 
 }
--- OOPs_Implementaton/src/in/trump/interfaces/impl/Bike.java
+++ OOPs_Implementaton/src/in/trump/interfaces/impl/Bike.java
@@ -10,10 +10,19 @@ public class Bike implements Vehicle {
 	public void serviceVehicle(int duration) {
 		System.out.println("get service done in : " +duration + " days");
 	}
-
+	
+//	@Override
+//	public void driveVehicle() { //The method driveVehicle() of type Bike must override or implement a supertype method
+//		System.out.println("Driver/Owner is driving");
+//	}
+	
 	public static void main(String[] args) {
 		Vehicle b = new Bike();
 		b.serviceVehicle(45);
         b.startVehicle();
+        //driveVehicle();
+       // b.driveVehicle(); //This static method of interface Vehicle can only be accessed as Vehicle.driveVehicle
+        Vehicle.driveVehicle();
+        
 	}
 }
