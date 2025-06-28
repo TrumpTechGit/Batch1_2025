5ee5a7d7ac0d89d70e12801243ed51c1826cf5f0
TrumpTechGit <Ramsamsani@Ram.mshome.net>
2025-06-28 08:45:22 +0530

    28/06/2025 - Wrapper classes

---
+++ OOPs_Implementaton/src/in/trump/wrapperclasses/WrapperDemo.java
@@ -0,0 +1,21 @@
+package in.trump.wrapperclasses;
+
+public class WrapperDemo {
+	
+	public static void main(String[] args) {
+		int a = (Integer)10;
+		//System.out.println(a);
+		
+		Integer x = new Integer(a);
+		System.out.println(x);
+		
+		String b = "120";
+		Integer y = new Integer(b);
+		System.out.println(y);
+		
+		
+		
+
+	}
+
+}
---
+++ OOPs_Implementaton/src/in/trump/wrapperclasses/WrapperDemo2.java
@@ -0,0 +1,20 @@
+package in.trump.wrapperclasses;
+
+public class WrapperDemo2 {
+
+	public static void main(String[] args) {
+		Integer a,b;
+		a = new Integer(10); 
+		a = 10; // auto boxing
+		b = 20;  // auto boxing
+		
+		int c = a; // auto unboxing
+		System.out.println(a+" "+b+" "+c);
+		
+		// Parsing
+		String numberString = "12345";
+		int number = Integer.parseInt(numberString);
+		System.out.println("parsed integer :"+number);
+		}
+
+}
